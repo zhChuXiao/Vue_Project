495836eb473eefe85abfb1627424d07fbca3190e
wangshuo7 <13665439520@163.com>
2022-11-09 12:03:43 +0800

    2

--- src/views/OnlineView.vue
+++ src/views/OnlineView.vue
@@ -1,15 +1,29 @@
 <template>
-<div class="serch">123</div>
+  <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form-item label="Approved by">
+      <el-input v-model="formInline.user" placeholder="Approved by" />
+    </el-form-item>
+    <el-form-item label="Activity zone">
+      <el-select v-model="formInline.region" placeholder="Activity zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Query</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { reactive } from 'vue'
 
-</script>
+const formInline = reactive({
+  user: '',
+  region: '',
+})
 
-<style lang="scss" scoped>
-.serch {
-  width: 100%;
-  height: 136px;
-  background: #fff;
+const onSubmit = () => {
+  console.log('submit!')
 }
-</style>
+</script>
