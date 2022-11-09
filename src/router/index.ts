0abebf607b75dc7c38fdb8385c0394f6f7ab0bc0
Chu Xiao <1437301462@qq.com>
2022-11-09 10:53:16 +0800

    chuxiao

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { ElMessage } from 'element-plus';
-import type {
-  Router,
-  RouteLocationNormalized,
-  NavigationGuardNext,
-} from 'vue-router';
+import type { Router, RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
 const router: Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -47,13 +43,13 @@ const router: Router = createRouter({
         },
         // 系统监控
         {
-          path:'/online',
-          name:'online',
-          component:():Promise<any> => import('@/views/OnlineView.vue')
-        }
+          path: '/online',
+          name: 'online',
+          component: (): Promise<any> => import('@/views/OnlineView.vue'),
+        },
       ],
     },
-    
+
     {
       path: '/login',
       name: 'login',
